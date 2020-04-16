349f1e9fc0a2e30b9f4d439d06561577b4fe0c60
RobinKardell <carden.rk@gmail.com>
2020-04-16 07:56:28 +0200

    Uppdaterade Databasen
    här kommer den enkla databasen.

--- phpbooking.sql
+++ phpbooking.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Värd: localhost
--- Tid vid skapande: 15 apr 2020 kl 18:55
--- Serverversion: 10.4.8-MariaDB
--- PHP-version: 7.3.11
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 16 apr 2020 kl 07:55
+-- Serverversion: 10.4.6-MariaDB
+-- PHP-version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,15 +32,19 @@ CREATE TABLE `bookings` (
   `id` int(11) NOT NULL,
   `name` varchar(150) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `timeslot` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumpning av Data i tabell `bookings`
 --
 
-INSERT INTO `bookings` (`id`, `name`, `email`, `date`) VALUES
-(1, 'Robin', 'test@test.com', '2020-04-15');
+INSERT INTO `bookings` (`id`, `name`, `email`, `date`, `timeslot`) VALUES
+(1, 'Robin', 'test@test.com', '2020-04-15', ''),
+(2, 'robin', 'test@test.co', '2020-04-24', ''),
+(3, 'Test 1.1.1', 'dansstallen@test.com', '2020-04-15', '13:30 - 14:00'),
+(4, 'MOA', 'ASFAF@FASF.SE', '2020-04-15', '11:30 - 12:00');
 
 --
 -- Index för dumpade tabeller
@@ -60,7 +64,7 @@ ALTER TABLE `bookings`
 -- AUTO_INCREMENT för tabell `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
